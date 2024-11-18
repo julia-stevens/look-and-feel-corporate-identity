32369cc624d14c8a9204eefc7dd32e2ff5ebb192
julia-stevens <juliastevens02@gmail.com>
2024-11-18 18:05:48 +0100

    #10 Menu radiozenders op mobiel

--- script/script.js
+++ script/script.js
@@ -5,6 +5,21 @@ navHamburger.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     const navMenu = document.querySelector("nav ul");
+    const navButton = document.querySelector(".nav-button img");
 
     navMenu.classList.toggle("showMenu");
+    navButton.classList.toggle("showMenu");
+}
+
+// Zenders menu 
+const buttonZenders = document.querySelector(".nav-zenders button");
+
+buttonZenders.addEventListener("click", toggleZenders);
+
+function toggleZenders() {
+    const navZenders = document.querySelector(".nav-zenders");
+    const buttonImgZenders = document.querySelector(".nav-zenders button img");
+
+    navZenders.classList.toggle("showZenders");
+    buttonImgZenders.classList.toggle("showZenders");
 }
